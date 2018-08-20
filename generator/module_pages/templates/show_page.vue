cb19393c2f179461c7ddb62ce002a3263dd4100f
Alexander Schwartzberg <aeksco@gmail.com>
2018-08-20 00:58:50 -0400

    Re-implementing relation views

--- generator/module_pages/templates/show_page.vue
+++ generator/module_pages/templates/show_page.vue
@@ -6,6 +6,19 @@
 
     <div class="row">
       TODo - relations go here
+      <%_ schema.relations.forEach((rel) => { _%>
+      <div class="col-lg-12">
+        <%_ if (rel.type === 'BELONGS_TO') { _%>
+        <p class='lead'><%= rel.alias.label %></p>
+        <<%= rel.alias.class_name %> />
+
+        <%_ } else if (rel.type === 'OWNS_MANY') { _%>
+        <p class='lead'><%= rel.alias.label_plural %></p>
+        <<%= rel.alias.class_name_plural %> />
+
+        <%_ } _%>
+      </div>
+      <%_ }) _%>
     </div>
 
   </div>
@@ -17,6 +30,13 @@
 import { mapGetters, mapActions } from 'vuex'
 import Loading from '@/components/Loading'
 import <%= schema.class_name %>ShowWidget from '@/modules/<%= schema.identifier %>/components/<%= schema.class_name %>ShowWidget'
+<%_ schema.relations.forEach((rel) => { _%>
+<%_ if (rel.type === 'BELONGS_TO') { _%>
+import <%= rel.alias.class_name %> from '@/modules/<%= schema.identifier %>/components/<%= rel.alias.class_name %>'
+<%_ } else if (rel.type === 'OWNS_MANY') { _%>
+import <%= rel.alias.class_name_plural %> from '@/modules/<%= schema.identifier %>/components/<%= rel.alias.class_name_plural %>'
+<%_ } _%>
+<%_ }) _%>
 
 export default {
   props: ['id'],
@@ -25,6 +45,13 @@ export default {
     title: '<%= schema.label %>s - Show'
   },
   components: {
+    <%_ schema.relations.forEach((rel) => { _%>
+    <%_ if (rel.type === 'BELONGS_TO') { _%>
+    <%= rel.alias.class_name %>,
+    <%_ } else if (rel.type === 'OWNS_MANY') { _%>
+    <%= rel.alias.class_name_plural %>,
+    <%_ } _%>
+    <%_ }) _%>
     <%= schema.class_name %>ShowWidget,
     Loading
   },
